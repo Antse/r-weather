3a3f13a4547e8d69dd5520cab6a92bbbb94bdb12
AntSe <antse@mbp>
2020-10-13 20:56:10 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use structopt::StructOpt;
+
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "request")]
+struct Opt {
+    #[structopt(short, long)]
+    city: String,
+    
+    #[structopt(short, long)]
+    app_key: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Wea {
+    main: String,
+    description: String,
+}
+
+#[derive(Deserialize, Debug)]
+struct Weather {
+    #[serde(rename = "name")]
+    ville: String,
+    weather: Vec<Wea>,
+}
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let opt = Opt::from_args();
+    let city_str = opt.city;
+    let app_key = opt.app_key;
+
+    let request_url = format!(
+        "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}",
+        city_str,
+        app_key
+    );
+
+    let response = reqwest::get(&request_url).await?.text().await?;
+
+    let w: Weather = serde_json::from_str(&response).unwrap();
+
+    println!(
+        "City -> {}\nWeather -> {}",
+        w.ville, w.weather[0].description
+    );
+    Ok(())
+}
